d719b5eda9f84be395cada7aade2c588d8f30568
luzzardi <luzzardi@gmail.com>
2021-09-10 00:45:09 -0300

    1 file modified

--- List/Todo.tsx
+++ List/Todo.tsx
@@ -1,44 +1,32 @@
 import React, { Component } from 'react';
 import { AppProps, TodoState } from '../States/';
 
-export default class Todo extends Component<AppProps, TodoState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: props.id,
-      name: props.name,
-      completed: props.completed,
-      toggleTaskCompleted: id => props.toggleTaskCompleted(id),
-      deleteTask: id => props.deleteTask(id)
-    };
-  }
-  render() {
-    return (
-      <li className="todo stack-small">
-        <div className="c-cb">
-          <input
-            id={this.state.id}
-            type="checkbox"
-            defaultChecked={this.state.completed}
-            onChange={() => this.state.toggleTaskCompleted(this.state.id)}
-          />
-          <label className="todo-label" htmlFor={this.state.id}>
-            {this.state.name}
-          </label>
-        </div>
-        <div className="btn-group">
-          <button type="button" className="btn">
-            Edit <span className="visually-hidden">{this.state.name}</span>
-          </button>
-          <button
-            type="button"
-            className="btn btn__danger"
-            onClick={() => this.state.deleteTask(this.state.id)}
-          >
-            Delete <span className="visually-hidden">{this.state.name}</span>
-          </button>
-        </div>
-      </li>
-    );
-  }
+export default function Todo(props) {
+  return (
+    <li className="todo stack-small">
+      <div className="c-cb">
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteTask(props.id)}
+        >
+          Delete <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
+    </li>
+  );
 }
